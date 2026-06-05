8f7f61977cbc3035c4aea460975b1cf225f524aa
MarcinWitekk <marcinwitekpl@outlook.com>
2026-06-05 12:39:45 +0200

    about page incoming

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -13,7 +13,7 @@ export default function About() {
                             width={150} 
                             height={150} 
                             loading="eager"
-                            className="" 
+                            className="logo" 
                             />
                         </div>
                     <div>
